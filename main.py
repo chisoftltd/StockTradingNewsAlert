671903e9acf7f3546a74174b2a1226c3e3e57ffe
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-13 10:08:20 +0000

    Using slice operator to get news article from news api

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ import requests
 import os
 from datetime import date, timedelta
 
-
-
 #   --------------------- CONSTANTS -----------------------
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 today = date.today()
-print(today)
 yesterday = today - timedelta(days=1)
-print(yesterday)
 daybe4 = today - timedelta(days=2)
-print(daybe4)
 
 stock_parameters = {
     "function": "TIME_SERIES_DAILY",
@@ -32,17 +27,19 @@ news_parameters = {
 stock_response = requests.get(url="https://www.alphavantage.co/query", params=stock_parameters)
 stock_response.raise_for_status()
 stock_data = stock_response.json()
-print(stock_data['Time Series (Daily)']['2022-01-11']['4. close'])
-print(stock_data['Time Series (Daily)']['2022-01-10']['4. close'])
-price_diff = float(stock_data['Time Series (Daily)']['2022-01-11']['4. close']) - float(
-    stock_data['Time Series (Daily)']['2022-01-10']['4. close'])
+yesterday_price = float(stock_data['Time Series (Daily)'][str(yesterday)]['4. close'])
+daybe4_price = float(stock_data['Time Series (Daily)'][str(daybe4)]['4. close'])
+price_diff = abs(yesterday_price - daybe4_price)
 print(price_diff)
-print((price_diff/float(stock_data['Time Series (Daily)']['2022-01-10']['4. close'])*100))
+percentage_price_diff = ((price_diff / daybe4_price) * 100)
+if percentage_price_diff > 2:
+    news_response = requests.get(url="https://newsapi.org/v2/everything", params=news_parameters)
+    news_response.raise_for_status()
+    news_data = news_response.json()
+    print(json.dumps(news_data["articles"][:3], indent=4))
 
 # print(json.dumps(data, indent=4))
 
 
-news_response = requests.get(url="https://newsapi.org/v2/everything", params=news_parameters)
-news_response.raise_for_status()
-news_data = news_response.json()
+
 # print(json.dumps(news_data, indent=4))
